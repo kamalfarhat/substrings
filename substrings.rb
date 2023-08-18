e5234b4903280fa855551c2300ff11b760a37c01
Kamal <kamalfarhat@gmail.com>
2023-08-18 23:43:45 +0800

    commit substrings.rb

--- substrings.rb
+++ substrings.rb
@@ -1,1 +1,15 @@
+def substrings(string, array)
 
+    string = string.downcase.split(' ')
+    my_dictionary = Hash.new(0)
+    array.each { |element|  
+        string.each { |s|
+        if s.include?element
+            my_dictionary[element] += 1
+        end
+        }
+    }
+    puts my_dictionary
+end
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("Howdy partner, sit down! How's it going?", dictionary)
